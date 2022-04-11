22cc122bf2fb945459e2a516e34dc4beec3ebb19
tfgl <tafanelgil@gmail.com>
2022-04-11 09:42:40 +0200

    update lsp

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -41,8 +41,14 @@ local config = {
   --
   -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
   init_options = {
-    bundles = {}
+    bundles = {
+      vim.fn.glob("~/bin/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.36.0.jar")
+    }
   },
+
+  on_attach = function(client, bufnr)
+    require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+  end
 }
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
